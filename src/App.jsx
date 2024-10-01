3bb63284957236af9cff1bfcd7ce9611cc3d68a8
Rishabh Dubey <rishabhdubey006@gmail.com>
2024-10-01 16:18:51 +0530

    fix some bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import './App.css'
+import Cards from './components/Cards'
+import Footer from './components/Footer'
+import Marquees from './components/Marquees'
+import StripesMain from './components/StripesMain'
 import Navbar from './components/topBar/Navbar'
+import Products from './components/topBar/Products'
+import Work from './components/Work'
+
+import LocomotiveScroll from 'locomotive-scroll';
+
+
 
 function App() {
+  const locomotiveScroll = new LocomotiveScroll();
   
   return (
     
-      <div className='w-full h-screen text-white  bg-zinc-800'>
+      <div className='w-full  text-white  bg-zinc-800'>
       
      <Navbar/>
+     <Work/>
+     <StripesMain/>
+     <Products/>
+     <Marquees/>
+     <Cards/>
+     <Footer/>
+
     </div>
   )
 }
